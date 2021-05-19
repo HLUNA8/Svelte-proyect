7a32be00c18223b1f0b951d70cf253c0df7bf1a6
HLUNA <lunahuascar@gmail.com>
2021-05-18 22:39:04 -0400

    se termino la configuracion de sapper

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,70 @@
+const webpack = require('webpack'); 
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const config = require('sapper/config/webpack.js');
+const pkg = require('./package.json')
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path : path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: ['*', '.mjs', '.js', '.svelte']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+
+const alias = { svelte: path.resolve('node_modules', 'svelte') };
+const extensions = ['.mjs', '.js', '.jason', '.svelte', '.html'];
+const mainFields = ['svelte', 'module', 'browser', 'main'];
+
+module.exports  = {
+    client: {
+        entry: config.client.entry(),
+        output: config.client.output(),
+        resolve: {alias, extensions, mainFields},
+        module: {
+            rules: [
+                {
+                    test: /\.(svelte|html)$/,
+                    use: {
+                        loader: 'svelte-loader',
+                        option: {
+                            dev,
+                            hydrateble: true,
+                            hotReload: false,
+
+                        }
+                    }
                 }
-            },
-            {
-                test: /\.svelte$/,
-                exclude: /node_modules/,
-                use:{
-                    loader: 'svelte-loader'
-                }
-            }
-        ]
+            ]
+        },
+        mode,
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.browser' : true,
+                'process.env.NODE_ENV' : JSON.stringify(mode)
+            }),
+        ].filter(Boolean),
+        devtool: dev && 'inline-source-map'
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            inject: true,
-            template:'./public/index.html',
-            filename:'./index.html',
-        })
-    ]
-}
+    server: {
+        entry: config.server.entry(),
+        output: config.server.output(),
+        target: 'node',
+        resolve: {alias, extensions, mainFields},
+        externals: Object.keys(pkd.despendencies).concat('encoding'),
+        module: {
+            rules: [
+                {
+                    test: /\.(svelte|html)$/,
+                    use: {
+                        loader: 'svelte-loader',
+                        option: {
+                            dev,
+                            css: false,
+                            generate: 'ssr'
+
+                        }
+                    }
+                }
+            ]
+        },
+        mode: process.env.NODE_ENV,
+        performance: {
+            hints: false,
+        }
+    },
+};
