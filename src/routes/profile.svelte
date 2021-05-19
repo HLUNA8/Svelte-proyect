ac252abdbb7f6d9c3b6f65dd91aacfd7f8e88ddd
HLUNA <lunahuascar@gmail.com>
2021-05-19 06:38:19 -0400

    se termino de generar las rutas principales para el proyecto

---
+++ src/routes/profile.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Main from '../components/Main.svelte';
+</script>
+
+<style>
+    .Profile {
+        padding: 4 em 0 0 0;
+    }
+</style>
+
+<svelte:head>
+    <title>Configuracion del Perfil </title>
+</svelte:head>
+
+<Main>
+    <div class="Profile">
+        <h2>Configuracion del Perfil</h2>
+    </div>
+</Main>>
--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,69 @@
-const webpack = require('webpack'); 
+const webpack = require('webpack');
 const path = require('path');
 const config = require('sapper/config/webpack.js');
-const pkg = require('./package.json')
+const pkg = require('./package.json');
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 
 const alias = { svelte: path.resolve('node_modules', 'svelte') };
-const extensions = ['.mjs', '.js', '.jason', '.svelte', '.html'];
+const extensions = ['.mjs', '.js', '.json', '.svelte', '.html'];
 const mainFields = ['svelte', 'module', 'browser', 'main'];
 
-module.exports  = {
-    client: {
-        entry: config.client.entry(),
-        output: config.client.output(),
-        resolve: {alias, extensions, mainFields},
-        module: {
-            rules: [
-                {
-                    test: /\.(svelte|html)$/,
-                    use: {
-                        loader: 'svelte-loader',
-                        option: {
-                            dev,
-                            hydrateble: true,
-                            hotReload: false,
-
-                        }
-                    }
-                }
-            ]
-        },
-        mode,
-        plugins: [
-            new webpack.DefinePlugin({
-                'process.browser' : true,
-                'process.env.NODE_ENV' : JSON.stringify(mode)
-            }),
-        ].filter(Boolean),
-        devtool: dev && 'inline-source-map'
-    },
-    server: {
-        entry: config.server.entry(),
-        output: config.server.output(),
-        target: 'node',
-        resolve: {alias, extensions, mainFields},
-        externals: Object.keys(pkd.despendencies).concat('encoding'),
-        module: {
-            rules: [
-                {
-                    test: /\.(svelte|html)$/,
-                    use: {
-                        loader: 'svelte-loader',
-                        option: {
-                            dev,
-                            css: false,
-                            generate: 'ssr'
-
-                        }
-                    }
-                }
-            ]
-        },
-        mode: process.env.NODE_ENV,
-        performance: {
-            hints: false,
+module.exports = {
+  client: {
+    entry: config.client.entry(),
+    output: config.client.output(),
+    resolve: { alias, extensions, mainFields },
+    module: {
+      rules: [
+        {
+          test: /\.(svelte|html)$/,
+          use: {
+            loader: 'svelte-loader',
+            options: {
+              dev,
+              hydratable: true,
+              hotReload: false
+            }
+          }
         }
+      ]
     },
+    mode,
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode)
+      }),
+    ].filter(Boolean),
+    devtool: dev && 'inline-source-map'
+  },
+
+  server: {
+    entry: config.server.entry(),
+    output: config.server.output(),
+    target: 'node',
+    resolve: { alias, extensions, mainFields },
+    externals: Object.keys(pkg.dependencies).concat('encoding'),
+    module: {
+      rules: [
+        {
+          test: /\.(svelte|html)$/,
+          use: {
+            loader: 'svelte-loader',
+            options: {
+              dev,
+              css: false,
+              generate: 'ssr'
+            }
+          }
+        }
+      ]
+    },
+    mode: process.env.NODE_ENV,
+    performance: {
+      hints: false
+    }
+  },
 };
